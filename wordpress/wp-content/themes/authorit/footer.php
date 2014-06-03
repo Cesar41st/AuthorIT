e8d27fbc339f5d9a775bcc6b2499add82848dadc
Belkord1 <belgarath_2@hotmail.fr>
2014-06-03 08:51:42 +0200

    style

--- wordpress/wp-content/themes/authorit/footer.php
+++ wordpress/wp-content/themes/authorit/footer.php
@@ -4,4 +4,28 @@
  * User: Bill
  * Date: 02/06/14
  * Time: 11:07
- */ 
+ */
+?>
+
+<footer>
+    <div class="container">
+        <div class="row">
+
+            <div class="presta col-md-4">
+                <h1>Qui sommes-nous ?</h1>
+                <p> Mentions légales</p>
+            </div>
+
+            <div class="presta col-md-4">
+                <h1>Le service</h1>
+                <p>Lorem ipsum</p>
+            </div>
+
+            <div class="presta col-md-4">
+                <h1>Condition</h1>
+                <p>Lorem ipsum</p>
+            </div>
+
+        </div>
+    </div>
+</footer>
