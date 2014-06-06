5d40ab61a0eb1598f7b48c214453e5e6f70dab09
Cesar41st <cesar.catherin@gmail.com>
2014-06-06 12:19:25 +0200

    fuck that

--- wordpress/wp-content/themes/authorit/templates/contact.php
+++ wordpress/wp-content/themes/authorit/templates/contact.php
@@ -7,6 +7,10 @@ Template Name: Contact
 
 <div id="contact">
 
+    <h2><i class="fa fa-envelope"></i> Contact us :</h2>
+    <img class="img-responsive" id="header_image" src="<?php bloginfo('template_directory'); ?>/image/header.jpg" alt="" title="">
+
+    <div id=""></div>
 
 
 </div>
